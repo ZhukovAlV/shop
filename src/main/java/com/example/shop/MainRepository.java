2600a71b4a091ac97cd592a82bfcf06f4a7d5ed0
Plinio <avzhykov@gmail.com>
2022-09-30 22:03:21 +0600

    Добавлен 6 урок

---
+++ src/main/java/com/example/shop/MainRepository.java
@@ -0,0 +1,7 @@
+package com.example.shop;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MainRepository {
+}
---
+++ src/main/java/com/example/shop/Product.java
@@ -0,0 +1,43 @@
+package com.example.shop;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "amount")
+    private String amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+}
