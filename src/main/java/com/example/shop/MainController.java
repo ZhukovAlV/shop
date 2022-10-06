927916c68faa38bbe559321c4d920d71b6fe5593
Plinio <avzhykov@gmail.com>
2022-10-06 20:19:04 +0600

    Добавлен репозиторий

--- src/main/java/com/example/shop/MainController.java
+++ src/main/java/com/example/shop/MainController.java
@@ -1,14 +1,26 @@
 package com.example.shop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String mainPage() {
+    private final MainRepository repo;
 
+    @Autowired
+    public MainController(MainRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/product")
+    public String mainPage(Model model) {
+
+        var result = repo.findAll();
+
+        model.addAttribute("products", result);
 
 
         return "product";
--- src/main/java/com/example/shop/MainRepository.java
+++ src/main/java/com/example/shop/MainRepository.java
@@ -1,7 +1,9 @@
 package com.example.shop;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MainRepository {
+public interface MainRepository extends CrudRepository<Product, Long> {
+
 }
