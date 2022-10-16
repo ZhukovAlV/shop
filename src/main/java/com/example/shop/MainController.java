0a84dd666ddf6af7d23bcd139dcdc92c42af6768
Plinio <avzhykov@gmail.com>
2022-10-16 12:54:46 +0600

    Добавлен шаблон сайта

--- src/main/java/com/example/shop/MainController.java
+++ src/main/java/com/example/shop/MainController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
 
@@ -15,8 +18,40 @@ public class MainController {
         this.repo = repo;
     }
 
-    @GetMapping("/product")
+
+    @GetMapping("/contact.html")
+    public String getContactPage() {
+        return "contact";
+    }
+
+    @GetMapping("/shop.html")
+    public String getShopPage(Model model) {
+
+/*        List<Product> allProducts = new ArrayList<>();
+
+        Product honey = new Product();
+        honey.setId(10L);
+        honey.setName("honey");
+        honey.setAmount(12);
+
+        Product propolis = new Product();
+        propolis.setId(11L);
+        propolis.setName("propolis");
+        propolis.setAmount(120);
+
+        allProducts.add(honey);
+        allProducts.add(propolis);
+
+        model.addAttribute("products", allProducts);*/
+
+        model.addAttribute("products", repo.findAll());
+
+        return "shop";
+    }
+
+ /*   @GetMapping("/product")
     public String mainPage(Model model) {
+
 
         var result = repo.findAll();
 
@@ -24,5 +59,5 @@ public class MainController {
 
 
         return "product";
-    }
+    } */
 }
