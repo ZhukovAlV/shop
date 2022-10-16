41c0401f3d7229187b0955fd57389ec398f9fe3b
Plinio <avzhykov@gmail.com>
2022-10-16 22:06:34 +0600

    Добавлен продукт

--- src/main/java/com/example/shop/MainConroller.java
+++ src/main/java/com/example/shop/MainConroller.java
@@ -1,7 +1,11 @@
 package com.example.shop;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainConroller {
@@ -12,7 +16,24 @@ public class MainConroller {
     }
 
     @GetMapping("/shop.html")
-    public String getShop() {
+    public String getShop(Model model) {
+
+        Product honey = new Product();
+        honey.setId(1);
+        honey.setName("Мёд");
+        honey.setAmount(100);
+
+        Product propolis = new Product();
+        propolis.setId(2);
+        propolis.setName("Прополис");
+        propolis.setAmount(200);
+
+        List products = new ArrayList();
+        products.add(honey);
+        products.add(propolis);
+
+        model.addAttribute("myValue", products);
+
         return "shop";
     }
 }
